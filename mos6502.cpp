43a16e71f614b1a4888f8072085cc616cd7b4b1c
wConnor <connorga4@gmail.com>
2022-11-03 22:43:34 +0000

    added cycle count to instr_matrix; implemented some basic ops.

--- mos6502.cpp
+++ mos6502.cpp
@@ -9,9 +9,9 @@ MOS6502::INSTRUCTIONINFO MOS6502::fetch_instruction(const std::uint8_t &instruct
 {
 	// searches for the instruction in the opcode matrix by its hexcode.
 	auto full_instruction = std::find_if(instr_matrix.begin(),
-							instr_matrix.end(),
-							[&instruction]
-							(const INSTRUCTIONINFO &instr_info) -> bool { return instruction == instr_info.op_code; });
+										 instr_matrix.end(),
+										 [&instruction]
+										 (const INSTRUCTIONINFO &instr_info) -> bool { return instruction == instr_info.op_code; });
 
 	// instruction found in matrix
 	return *full_instruction;
@@ -22,9 +22,389 @@ void MOS6502::decode_instruction(const INSTRUCTIONINFO &full_instruction)
 
 }
 
-void MOS6502::exec_instruction(const std::uint8_t &instruction)
+void MOS6502::exec_instruction(const INSTRUCTION &instruction, const std::uint16_t &address)
 {
+	switch(instruction) {
+	case INSTRUCTION::ADC:
 
+		break;
+	case INSTRUCTION::AND:
+		acc &= mem[address];
+
+		if (acc == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((acc >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::ASL:
+
+		break;
+	case INSTRUCTION::BCC:
+
+		break;
+	case INSTRUCTION::BCS:
+
+		break;
+	case INSTRUCTION::BEQ:
+
+		break;
+	case INSTRUCTION::BIT:
+
+		break;
+	case INSTRUCTION::BMI:
+
+		break;
+	case INSTRUCTION::BNE:
+
+		break;
+	case INSTRUCTION::BPL:
+
+		break;
+	case INSTRUCTION::BRK:
+		stack.push(pc);
+		stack.push(sr);
+		pc = 0xFFFE;
+		sr |= 0b00010000;
+
+		break;
+	case INSTRUCTION::BVC:
+
+		break;
+	case INSTRUCTION::BVS:
+
+		break;
+	case INSTRUCTION::CLC:
+		sr &= 0b11111110;
+		pc++;
+
+		break;
+	case INSTRUCTION::CLD:
+		sr &= 0b11110111;
+		pc++;
+
+		break;
+	case INSTRUCTION::CLI:
+		sr &= 0b11111011;
+		pc++;
+
+		break;
+	case INSTRUCTION::CLV:
+		sr &= 0b10111111;
+		pc++;
+
+		break;
+	case INSTRUCTION::CMP:
+		if (acc >= mem[address]) {
+			sr |= 0b00000001;
+		}
+
+		if (acc == mem[address]) {
+			sr |= 0b00000010;
+		}
+
+		if (((acc - mem[address]) >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::CPX:
+		if (acc >= mem[address]) {
+			sr |= 0b00000001;
+		}
+
+		if (acc == mem[address]) {
+			sr |= 0b00000010;
+		}
+
+		if (((ix - mem[address])) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::CPY:
+		if (acc >= mem[address]) {
+			sr |= 0b00000001;
+		}
+
+		if (acc == mem[address]) {
+			sr |= 0b00000010;
+		}
+
+		if (((iy - mem[address])) & 1) {
+			sr |= 0b10000000;
+		}
+
+		break;
+	case INSTRUCTION::DEC:
+		mem[address]--;
+		if (mem[address] == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((mem[address] >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::DEX:
+
+		break;
+	case INSTRUCTION::DEY:
+
+		break;
+	case INSTRUCTION::EOR:
+
+		break;
+	case INSTRUCTION::INC:
+
+		break;
+	case INSTRUCTION::INX:
+
+		break;
+	case INSTRUCTION::INY:
+
+		break;
+	case INSTRUCTION::JMP:
+		pc = address;
+
+		break;
+	case INSTRUCTION::JSR:
+		stack.push(pc - 1);
+		pc = address;
+
+		break;
+	case INSTRUCTION::LDA:
+		acc = mem[address];
+
+		if (acc == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((acc >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::LDX:
+		ix = mem[address];
+
+		if (ix == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((ix >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::LDY:
+		iy = mem[address];
+
+		if (iy == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((iy >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::LSR:
+
+		break;
+	case INSTRUCTION::NOP:
+		pc++;
+
+		break;
+	case INSTRUCTION::ORA:
+		acc |= mem[address];
+
+		if (acc == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((acc >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		break;
+	case INSTRUCTION::PHA:
+		stack.push(acc);
+		pc++;
+
+		break;
+	case INSTRUCTION::PHP:
+		stack.push(sr);
+		pc++;
+
+		break;
+	case INSTRUCTION::PLA:
+		acc = stack.top();
+		stack.pop();
+
+		if (acc == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((acc >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::PLP:
+		sr = stack.top();
+		stack.pop();
+
+		pc++;
+
+		break;
+	case INSTRUCTION::ROL:
+
+		break;
+	case INSTRUCTION::ROR:
+
+		break;
+	case INSTRUCTION::RTI:
+
+		break;
+	case INSTRUCTION::RTS:
+		pc = stack.top();
+		stack.pop();
+
+		break;
+	case INSTRUCTION::SBC:
+
+		break;
+	case INSTRUCTION::SEC:
+		sr |= 0b00000001;
+		pc++;
+
+		break;
+	case INSTRUCTION::SED:
+		sr |= 0b00001000;
+		pc++;
+
+		break;
+	case INSTRUCTION::SEI:
+		sr |= 0b00000100;
+		pc++;
+
+		break;
+	case INSTRUCTION::STA:
+		mem[address] = acc;
+		pc++;
+
+		break;
+	case INSTRUCTION::STX:
+		mem[address] = ix;
+		pc++;
+
+		break;
+	case INSTRUCTION::STY:
+		mem[address] = iy;
+		pc++;
+
+		break;
+	case INSTRUCTION::TAX:
+		ix = acc;
+
+		if (ix == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((ix >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::TAY:
+		iy = acc;
+
+		if (iy == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((iy >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::TSX:
+		ix = sp;
+
+		if (ix == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((ix >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::TXA:
+		acc = ix;
+
+		if (acc == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((acc >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+	case INSTRUCTION::TXS:
+		sp = ix;
+		pc++;
+
+		break;
+	case INSTRUCTION::TYA:
+		acc = iy;
+
+		if (acc == 0) {
+			sr |= 0b00000010;
+		}
+
+		if ((acc >> 7) & 1) {
+			sr |= 0b10000000;
+		}
+
+		pc++;
+
+		break;
+
+	default:
+
+		break;
+	}
 }
 
 MOS6502::~MOS6502()
