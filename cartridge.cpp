a43ff493b6fbbf1eabcf3ec7fed89677e27ddfc9
wConnor <connorga4@gmail.com>
2022-11-10 23:32:44 +0000

    added std::string initialiser for cartridges; 'make run' in cmake.

--- cartridge.cpp
+++ cartridge.cpp
@@ -1,22 +1,39 @@
 #include "cartridge.hpp"
 
+// initialiser for if the user wishes to pass a string literal. primarily for
+// debugging.
+Cartridge::Cartridge(const std::string &temp)
+{
+	this->temp = temp;
+}
+
+// initialiser for passing a .BIN / .ROM file (such as a game or 6502 program).
 Cartridge::Cartridge(const std::filesystem::path &rom)
 {
 	this->loaded_cartridge = rom;
 }
 
+// loads the contents of either the given ROM file or the string literal into
+// the contents std::array for use by the NES.
 std::shared_ptr<std::array<std::uint8_t, MAX_CARTRIDGE_SIZE>> Cartridge::load()
 {
-	std::ifstream file(this->loaded_cartridge, std::ios::in | std::ios::binary);
-	auto contents = std::make_shared<std::array<std::uint8_t, MAX_CARTRIDGE_SIZE>>();
+	// implies that a ROM path has been passed instead of a string literal.
+	if (this->temp.empty()) {
+		std::ifstream file(this->loaded_cartridge, std::ios::in | std::ios::binary);
+		this->contents = std::make_shared<std::array<std::uint8_t, MAX_CARTRIDGE_SIZE>>();
 
-	while (file.good()) {
-		for (int i = 0; i != MAX_CARTRIDGE_SIZE; ++i) {
-			file >> (*contents)[i];
+		while (file.good()) {
+			for (int i = 0; i != MAX_CARTRIDGE_SIZE; ++i) {
+				file >> (*this->contents)[i];
+			}
+		}
+	} else {
+		for (int i = 0; i != temp.size(); ++i) {
+			(*this->contents)[i] = temp[i];
 		}
 	}
 
-	return contents;
+	return this->contents;
 }
 
 Cartridge::~Cartridge()
--- cartridge.hpp
+++ cartridge.hpp
@@ -10,12 +10,15 @@ constexpr std::uint16_t MAX_CARTRIDGE_SIZE = 0xFFFF;
 class Cartridge
 {
 public:
+	Cartridge(const std::string &contents);
 	Cartridge(const std::filesystem::path &rom);
 	std::shared_ptr<std::array<std::uint8_t, MAX_CARTRIDGE_SIZE>> load();
 	virtual ~Cartridge();
 
 private:
 	std::filesystem::path loaded_cartridge;
+	std::shared_ptr<std::array<std::uint8_t, MAX_CARTRIDGE_SIZE>> contents;
+	std::string temp = "";
 };
 
 #endif
